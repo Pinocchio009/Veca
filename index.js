8e4546f4da6c23941b45875c964919f45d2dfa8e
pinocchio <81025400+Pinocchio009@users.noreply.github.com>
2022-06-28 15:45:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { json } = require("express");
 const flights = require("./controllers/flightController");
-//const models = require("./models/Flight");
+const models = require("./models/Flight");
 const Flight = require('./routes/flightRoute');
 
 const app = express();
